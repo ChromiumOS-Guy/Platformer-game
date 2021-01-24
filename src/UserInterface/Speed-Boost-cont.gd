09344b0227e8aa1d79926f20e3182387e6abbefb
alonarvatz <57168079+alonarvatz@users.noreply.github.com>
2021-01-24 09:39:12 +0200

    Delete src directory

--- src/UserInterface/Speed-Boost-cont.gd
+++
@@ -1,9 +0,0 @@
-#author Alon Arvatz
-#Github https://github.com/alonarvatz/Platformer-game
-
-
-
-extends Label
-var a = str(Database.SpeedBoostcap)
-func _ready() -> void:
-	set_text("Speed-Boosts: %s" % a)
--- src/scripts/coin-counter.gd
+++
@@ -1,9 +0,0 @@
-#author Alon Arvatz
-#Github https://github.com/alonarvatz/Platformer-game
-
-
-
-extends Label
-var a = str(Database.coinsave)
-func _process(_delta):
-	set_text(a)
