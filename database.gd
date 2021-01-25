3b8f3c2bffd1abdcdb7aea7aa50ed0b5c5713d3f
alonarvatz <57168079+alonarvatz@users.noreply.github.com>
2021-01-25 10:02:47 +0200

    Add files via upload

---
+++ database.gd
@@ -0,0 +1,30 @@
+#author Alon Arvatz
+#Github https://github.com/alonarvatz/Platformer-game
+
+
+
+extends Node
+
+var dir = Directory
+var coin = 0
+var coinsave = 0
+var havecoins = false
+var reload = true
+var SpeedBoost = 3500
+var SpeedBoostcap = 0
+var SpeedBoostnum = true
+var shit = 0
+var lol = false
+var tranformx = 0.000
+var dis = false
+var vel = 0.00
+var bossdamage = false
+func load_game():
+	GameSaver.Load()
+	print("loaded game")
+	
+
+
+func savegame():
+	GameSaver.save()
+	print("saved game")
